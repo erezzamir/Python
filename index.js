7a112ad9a7f5bee8115bbda8d05bf9e20528a811
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-03 10:02:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client();
 
 const swearWords = ["fuck", "bitch"];
 
