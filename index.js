617a0d717ee844dbea92985f2ee28d1778fcf053
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-03 11:05:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on("ready", async () => {
+  console.log(`Bot is Online!`);
 client.user.setGame("!help | Created by Derpy");
 });
 
